46f902f26a5c45b4485f9b15937db798e0d40b06
Sujan299 <mrsujan321@gmail.com>
2025-02-25 22:46:32 +0545

    last commit

--- client/src/components/GenAI.jsx
+++ client/src/components/GenAI.jsx
@@ -12,10 +12,13 @@ const GenAI = ({ mainTheme }) => {
       <h1 className='md:text-[3vw] xl:text-[2vw] text-2xl font3'>Articles</h1>
       <div className='pt-[4vh]'>
         {
-          items.length >=1 ? <ArticleComponent data={items}/> : <><div className="space-y-4 h-[80vh]">
-          <div className="h-48 bg-gray-400 rounded-md animate-pulse"></div>
-          <div className="h-48 bg-gray-400 rounded-md animate-pulse"></div>
-        </div></>
+          items.length >= 1 ? <ArticleComponent data={items} /> : <>
+            {/* <div className="space-y-4 h-[80vh]">
+            <div className={`h-48 bg-gray-400 rounded-md animate-pulse`}></div>
+            <div className={`h-48 bg-gray-400 rounded-md animate-pulse`}></div>
+          </div> */}
+          <h2 className='text-center'>No articles yet !</h2>
+          </>
         }
       </div>
     </div>
